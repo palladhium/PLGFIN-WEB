f82b7cd69cb1ec1c07c830dc1b820f9dc4137633
Palladhium <palladhium@outlook.com.br>
2020-10-01 22:57:00 -0300

    PLGFIN-15182020093045
    - Implementar integração do frontend com o backend
    - Implementar ajustes no backend
    - Implementar funcionalidade de Gerenciar Ciclo de Pagamentos

---
+++ application/module/gerenciador-monitoramento/gerenciador-monitoramento-controller.js
@@ -0,0 +1,21 @@
+(function () {
+
+    angular.module("PlataformaLancamentoGerenciadorFinanceiroModule").controller("monitoramentoController", [
+        "$http",
+        monitoramentoController
+    ])
+
+    function monitoramentoController($http) {
+        const vm = this;
+        vm.resumoFaturamento = function () {
+            const URL_PLGFIN_API = "http://localhost:9090/api/resumo-faturamento";
+            $http.get(URL_PLGFIN_API).then(function (response) {
+                vm.total_credito = response.data.id_credito;
+                vm.total_debito = response.data.id_debito;
+                vm.total_geral = vm.total_credito - vm.total_debito;
+            });
+        };
+        vm.resumoFaturamento();
+    }
+
+})();
